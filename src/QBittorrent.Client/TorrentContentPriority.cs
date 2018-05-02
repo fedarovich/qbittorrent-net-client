c8edda78fb8f4b0fd5f672d24ba17b05b734933d
Pavel Fedarovich <p.fedarovich@gmail.com>
2018-05-02 23:45:40 +0300

    Changed TorrentContentPriority enum.

--- src/QBittorrent.Client/TorrentContentPriority.cs
+++ src/QBittorrent.Client/TorrentContentPriority.cs
@@ -6,22 +6,42 @@
     public enum TorrentContentPriority
     {
         /// <summary>
-        /// Do not download the file.
+        /// Do not download the file (0).
         /// </summary>
         Skip = 0,
-        
-        /// <summary>
-        /// The normal priority.
-        /// </summary>
-        Normal = 1,
-        
-        /// <summary>
-        /// The high priority.
-        /// </summary>
-        High = 6,
 
         /// <summary>
-        /// The maximal priority.
+        /// The minimal priority (1). 
+        /// </summary>
+        Minimal = 1,
+
+        /// <summary>
+        /// The very low priority (2).
+        /// </summary>
+        VeryLow = 2,
+
+        /// <summary>
+        /// The low priority (3).
+        /// </summary>
+        Low = 3,
+
+        /// <summary>
+        /// The normal priority (4).
+        /// </summary>
+        Normal = 4,
+
+        /// <summary>
+        /// The high priority (5).
+        /// </summary>
+        High = 5,
+
+        /// <summary>
+        /// The very high priority (6).
+        /// </summary>
+        VeryHigh = 6,
+        
+        /// <summary>
+        /// The maximal priority (7).
         /// </summary>
         Maximal = 7
     }

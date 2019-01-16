e5c36607368c262c4babc89bb5a166dfdfc58a27
Pavel Fedarovich <p.fedarovich@gmail.com>
2019-01-16 23:45:54 +0300

    Added missing XML documentation comments.

--- src/QBittorrent.Client/RssFolder.cs
+++ src/QBittorrent.Client/RssFolder.cs
@@ -13,24 +13,49 @@ namespace QBittorrent.Client
     [DebuggerDisplay("Folder {Name} with {Items.Count} items")]
     public class RssFolder : RssItem
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RssFolder"/> class.
+        /// </summary>
         public RssFolder() : this(null)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RssFolder"/> class.
+        /// </summary>
+        /// <param name="items">The folder items.</param>
         public RssFolder(IEnumerable<RssItem> items) : this(string.Empty, items)
         {
         }
 
+        /// <summary>Initializes a new instance of the <see cref="RssFolder"/> class.</summary>
+        /// <param name="name">The folder name.</param>
+        /// <param name="items">The folder items.</param>
         public RssFolder(string name, IEnumerable<RssItem> items)
         {
             Name = name;
             Items = new List<RssItem>(items ?? Enumerable.Empty<RssItem>());
         }
 
+        /// <summary>
+        /// Gets all folder items (feeds and folders).
+        /// </summary>
+        /// <seealso cref="Feeds"/>
+        /// <seealso cref="Folders"/>
         public IList<RssItem> Items { get; }
 
+        /// <summary>
+        /// Gets all subfolders.
+        /// </summary>
+        /// <seealso cref="Items"/>
+        /// <seealso cref="Feeds"/>
         public IEnumerable<RssFolder> Folders => Items?.OfType<RssFolder>();
 
+        /// <summary>
+        /// Gets all feeds in the folder.
+        /// </summary>
+        /// <seealso cref="Items"/>
+        /// <seealso cref="Folders"/>
         public IEnumerable<RssFeed> Feeds => Items?.OfType<RssFeed>();
     }
 }
